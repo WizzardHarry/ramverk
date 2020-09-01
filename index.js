8ee52fdbfda116a6eff8cd0969bdd827b5c2a427
WizzardHarry <58033868+WizzardHarry@users.noreply.github.com>
2020-09-01 11:28:50 +0300

    added new paragraph
    Added another paragraph testing github

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'your name here',
+        secondmessage: "This is a message written with vue"
+    }
+});
+
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'Du laddade sidan:' + new Date().toLocaleString()
+    }
+  })
+
+  //this is a comment
+  //this is another comment
+
+  var app3 = new Vue ({
+    el: '#app-3',
+    data: {
+      message3: ' testar github ändringar:'
+    }
+  })

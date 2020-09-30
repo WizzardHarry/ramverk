d64bf10066917b1bfde68a025410acfa39eaabdb
Herlin-crtl-c <70571487+Herlin-crtl-c@users.noreply.github.com>
2020-09-30 11:02:44 +0300

    la lite till i feedbacken
    Tar nu automatiskt From och To från tidigare + alert och reload

--- script/index.js
+++ script/index.js
@@ -33,3 +33,11 @@ var app2 = new Vue({
                                             // OBS! Endast one way bind. Använderen kan inte modifiera fältet från en input
     }
   })
+  
+  var sub = new Vue({
+    el: "submitbutton",
+    data: {
+      message5: "test"
+    }
+  })
+  
